58198f4cf0be8307e48d9854e4fa0be748b6cf65
Gene <154293262+CryptainGolD@users.noreply.github.com>
2024-12-23 12:19:06 +0900

    Initial commit

---
+++ src/utils/routes.ts
@@ -0,0 +1,9 @@
+export const LEIZD = ''
+export const DEV_CENTRAL = 'https://leizd.com/devcentral'
+export const REPOSITORY =
+  'https://github.com/horizonx-tech/aptos-module-explorer'
+
+export const DOCUMENTS = ''
+export const TWITTER = 'https://twitter.com/LeizdProtocol'
+export const DISCORD = ''
+export const GITHUB = 'https://github.com/horizonx-tech'
